0bc65b0beada92a3c5b57c74df06695b873a4e3b
ShujaARizvi <shuja1515@gmail.com>
2020-01-24 23:47:38 +0500

    Initial commit

---
+++ app/build/generated/source/buildConfig/release/com/syncbros/keepcounter/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.syncbros.keepcounter;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.syncbros.keepcounter";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
